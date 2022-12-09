5fb9478eec3d1782c60d8669e6645cf49202cd0d
minyjae <allmusictube@gmail.com>
2022-12-09 16:39:13 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,26 @@
 using namespace std;
 
 int main(){
+    int x;
+    int o=0;
+    int e=0;
+    do {
     cout << "Enter an integer: ";
+    cin >> x;
+    if (x %2 == 0 && x != 0) {
+        e++;
+    }
+    else if (x %2 != 0 && x !=0) {
+        o++;
+    }
+    } while (x != 0) ;
+    
+
     cout << "#Even numbers = ";
+    cout << e << endl;
     cout << "#Odd numbers = ";
+    cout << o;
     return 0;
 }
+   
+   
